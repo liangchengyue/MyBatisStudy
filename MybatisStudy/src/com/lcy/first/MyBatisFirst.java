e631d9de0b15d12e526cdcea529baa5672792d61
梁城月 <2524499314@qq.com>
2017-08-05 18:48:27 +0800

    添加用户

--- MybatisStudy/src/com/lcy/first/MyBatisFirst.java
+++ MybatisStudy/src/com/lcy/first/MyBatisFirst.java
@@ -2,6 +2,7 @@ package com.lcy.first;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -50,6 +51,28 @@ public class MyBatisFirst {
 		// list中的user和映射文件中resultType所指定的类型一致
 		List<User> list = sqlSession.selectList("test.findUserByName", "小明");
 		System.out.println(list);
+		sqlSession.close();
+	}
+	@Test
+	public void insertUser() throws IOException {
+		// mybatis配置文件
+		String resource = "SqlMapConfig.xml";
+		// 得到配置文件流
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		// 创建会话工厂，传入mybatis的配置文件信息
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(inputStream);
+		// 通过工厂得到SQLSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		// list中的user和映射文件中resultType所指定的类型一致
+		User user=new User();
+		user.setUsername("王小丫");
+		user.setSex("女");
+		user.setBirthday(new Date());
+		user.setAddress("贵州贵阳");
+		sqlSession.insert("test.insertUser", user);
+		//提交事务
+		sqlSession.commit();
 		sqlSession.close();
 	}
 }
