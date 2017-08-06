199e21f6f50c8dbc2433cffa1a6ad149d6587685
梁城月 <2524499314@qq.com>
2017-08-06 19:07:58 +0800

    Mapper代理

---
+++ MybatisStudy/src/com/lcy/pojo/UserCustom.java
@@ -0,0 +1,9 @@
+package com.lcy.pojo;
+/**
+ * 用户扩展类
+ * @author 梁城月
+ *
+ */
+public class UserCustom extends User {
+
+}
---
+++ MybatisStudy/src/com/lcy/vo/UserQuer.java
@@ -0,0 +1,23 @@
+package com.lcy.vo;
+
+import com.lcy.pojo.User;
+import com.lcy.pojo.UserCustom;
+
+/**
+ * 包装类型
+ * 
+ * @author 梁城月
+ * 
+ */
+public class UserQuer {
+	private UserCustom userCustom;
+
+	public UserCustom getUserCustom() {
+		return userCustom;
+	}
+
+	public void setUserCustom(UserCustom userCustom) {
+		this.userCustom = userCustom;
+	}
+
+}
